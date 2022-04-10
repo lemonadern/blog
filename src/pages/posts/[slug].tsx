d60d7a5bb26834509ee833e9b94e040997cdfe83
lemonadern <taishi.develop@gmail.com>
2022-04-10 14:32:37 +0900

    remove author and coverImage

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -8,7 +8,6 @@ import Layout from '@/components/layout'
 import { getPostBySlug, getAllPosts } from '@/lib/api'
 import PostTitle from '@/components/post-title'
 import Head from 'next/head'
-import { CMS_NAME } from '@/lib/constants'
 import markdownToHtml from '@/lib/markdownToHtml'
 import PostType from '@/types/post'
 
@@ -34,15 +33,13 @@ const Post = ({ post, morePosts }: Props) => {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.title} | lemonadern's blog
                 </title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
               <PostHeader
                 title={post.title}
-                coverImage={post.coverImage}
                 date={post.date}
-                author={post.author}
               />
               <PostBody content={post.content} />
             </article>
