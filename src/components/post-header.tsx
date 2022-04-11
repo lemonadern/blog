af40624f0b7dcdc82ed6c89a35176cd0e50ddaf7
lemonadern <taishi.develop@gmail.com>
2022-04-12 06:27:14 +0900

    change date appearance

--- src/components/post-header.tsx
+++ src/components/post-header.tsx
@@ -10,10 +10,8 @@ const PostHeader = ({ title, date }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="flex justify-end max-w-2xl mx-auto">
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
+      <div className="flex justify-center gap-6 text-opacity-80 max-w-4xl mx-auto mb-14">
+        <DateFormatter dateString={date} />
       </div>
     </>
   )
