68bc8a7e3696ff9057d77cedfc3aba4146324bdf
lemonadern <taishi.develop@gmail.com>
2022-04-12 05:54:19 +0900

    add footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,19 @@
 import Container from './container'
+import { TWITTER_URL, GITHUB_URL, ZENN_URL,WANTEDLY_URL } from '@/lib/constants'
+import { FaTwitter ,FaGithub } from "react-icons/fa"
+import { SiZenn } from 'react-icons/si'
 
 const Footer = () => {
   return (
     <footer className="py-6">
       <Container>
-        <div className="py-4 flex flex-row justify-center gap-2 items-center">
-          <p>Twitter</p>
-          <p>GitHub</p>
+        <div className="py-4 flex flex-row justify-center gap-6 items-center text-lg">
+          <a href={TWITTER_URL}><FaTwitter /></a>
+          <a href={GITHUB_URL}><FaGithub /></a>
+          <a href={ZENN_URL}><SiZenn /></a>
         </div>
         <div className="py-2">
-          <p className="text-center">&copy; 2022 lemonadern</p>
+          <p className="text-center text-base">&copy; 2022 lemonadern</p>
         </div>
       </Container>
     </footer>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,8 @@
 export const HOME_OG_IMAGE_URL =
   'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
 export const SITE_NAME = 'lemonadern\'s blog'
+
+export const TWITTER_URL = 'https://twitter.com/lemonadern'
+export const GITHUB_URL = 'https://github.com/lemonadern'
+export const ZENN_URL = 'https://zenn.dev/lemonadern'
+export const WANTEDLY_URL = 'https://www.wantedly.com/id/taishi_naka'
